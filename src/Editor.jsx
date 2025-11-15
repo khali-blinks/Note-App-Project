80cb8e89bb914f78a5d53230e5fea241e1011b4a
Khalifa <kismaiel61@gmail.com>
2025-11-15 16:50:37 -0500

    Items aligned on screen

---
+++ src/Editor.jsx
@@ -0,0 +1,53 @@
+import { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import "bulma/css/bulma.min.css";
+
+export default function Editors() {
+  const editorRef = useRef(null);
+
+  return (
+    <div className="is-flex is-justify-content-center is-align-items-center">
+      <>
+        <Editor
+          apiKey="poybbk4udfgq3b95i7w3gq8222uqjh66wooovresv5s43bde"
+          onInit={(_evt, editor) => (editorRef.current = editor)}
+          initialValue="<p>This is the initial content of the editor.</p>"
+          init={{
+            height: 500,
+            width: 800,
+            menubar: false,
+            min_height: 300,
+            max_height: 800,
+            plugins: [
+              "advlist",
+              "autolink",
+              "lists",
+              "link",
+              "image",
+              "charmap",
+              "preview",
+              "anchor",
+              "searchreplace",
+              "visualblocks",
+              "code",
+              "fullscreen",
+              "insertdatetime",
+              "media",
+              "table",
+              "code",
+              "help",
+              "wordcount",
+            ],
+            toolbar:
+              "undo redo | blocks | " +
+              "bold italic forecolor | alignleft aligncenter " +
+              "alignright alignjustify | bullist numlist outdent indent | " +
+              "removeformat | help",
+            content_style:
+              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          }}
+        />
+      </>
+    </div>
+  );
+}
