6662cca4f7fbac071fa9ea25bb74444511289b72
Khalifa <kismaiel61@gmail.com>
2025-11-15 10:54:11 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import "./App.css";
+
+export default function App() {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+
+  return (
+    <>
+      <Editor
+        apiKey="poybbk4udfgq3b95i7w3gq8222uqjh66wooovresv5s43bde"
+        onInit={(_evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "code",
+            "help",
+            "wordcount",
+          ],
+          toolbar:
+            "undo redo | blocks | " +
+            "bold italic forecolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
+    </>
+  );
+}
