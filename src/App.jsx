f3e2c07d0977f0c6d54a1c5fed5646da8b503f54
Khalifa <kismaiel61@gmail.com>
2025-11-15 16:51:14 -0500

    centered items

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import { useRef } from "react";
-import { Editor } from "@tinymce/tinymce-react";
+import Editors from "./Editor";
 import "bulma/css/bulma.min.css";
 
 export default function App() {
-  const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
-  };
-
   return (
     <>
       <section className="hero is-primary mb-5">
@@ -17,48 +9,10 @@ export default function App() {
           <p className="title">Welcome to My Note App</p>
         </div>
       </section>
-      <button className="mb-5" onClick={log}>
-        New Note
-      </button>
-      <Editor
-        apiKey="poybbk4udfgq3b95i7w3gq8222uqjh66wooovresv5s43bde"
-        onInit={(_evt, editor) => (editorRef.current = editor)}
-        initialValue="<p>This is the initial content of the editor.</p>"
-        init={{
-          height: 500,
-          width: 800,
-          menubar: false,
-          min_height: 300,
-          max_height: 800,
-          plugins: [
-            "advlist",
-            "autolink",
-            "lists",
-            "link",
-            "image",
-            "charmap",
-            "preview",
-            "anchor",
-            "searchreplace",
-            "visualblocks",
-            "code",
-            "fullscreen",
-            "insertdatetime",
-            "media",
-            "table",
-            "code",
-            "help",
-            "wordcount",
-          ],
-          toolbar:
-            "undo redo | blocks | " +
-            "bold italic forecolor | alignleft aligncenter " +
-            "alignright alignjustify | bullist numlist outdent indent | " +
-            "removeformat | help",
-          content_style:
-            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-        }}
-      />
+      <div className="buttons is-centered">
+        <button className=" button is-link mb-5">New Note</button>
+      </div>
+      <Editors />
     </>
   );
 }
