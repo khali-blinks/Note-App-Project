d97f21fa527a2d5910f8f8b4f14bdb813940c72a
Khalifa <kismaiel61@gmail.com>
2025-11-26 18:25:28 -0500

    Second Commit

--- src/Basic Data Structures.js
+++ src/Basic Data Structures.js
@@ -7,3 +7,16 @@ function mixedNumbers(arr) {
 }
 
 console.log(mixedNumbers(["IV", 5, "six"]));
+
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    // Only change code below this line
+    newArr.push([...arr]);
+    // Only change code above this line
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
