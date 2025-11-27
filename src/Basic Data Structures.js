cf636aaa4384c01c69a53e0d433dbfe18fbc51fd
Khalifa <kismaiel61@gmail.com>
2025-11-27 16:31:38 -0500

    DSA

--- src/Basic Data Structures.js
+++ src/Basic Data Structures.js
@@ -40,3 +40,28 @@ let myNestedArray = [
   ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth"],
   // Only change code above this line
 ];
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: true,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+function isEveryoneHere(userObj) {
+  return ["Alan", "Jeff", "Sarah", "Ryan"].every((name) =>
+    userObj.hasOwnProperty(name)
+  );
+}
