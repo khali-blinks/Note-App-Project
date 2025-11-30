4800862561cab1e287c191c96bf9faf714ed1beb
Khalifa <kismaiel61@gmail.com>
2025-11-30 10:05:51 -0500

    FindersKeepers

--- src/Basic Data Structures.js
+++ src/Basic Data Structures.js
@@ -143,3 +143,18 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+function findElement(arr, func) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num) === true) {
+      break;
+    } else {
+      num = undefined;
+    }
+  }
+  return num;
+}
+
+findElement([1, 2, 3, 4], (num) => num % 2 === 0);
