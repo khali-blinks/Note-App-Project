1ea776b6b68b78ae8d39a9024a7632d9eb01338c
Khalifa <kismaiel61@gmail.com>
2025-12-01 19:11:34 -0500

    Slice and Splice

--- src/Basic Data Structures.js
+++ src/Basic Data Structures.js
@@ -172,3 +172,11 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+function frankenSplice(arr1, arr2, n) {
+  let newArr = [...arr2];
+  newArr.splice(n, 0, ...arr1);
+  return newArr;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
