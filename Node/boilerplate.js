b1f9a308f19a7a0f538656126c585afecf0d0cff
Khalifa <kismaiel61@gmail.com>
2025-12-07 08:17:46 -0500

    Using try catch

--- Node/boilerplate.js
+++ Node/boilerplate.js
@@ -2,3 +2,11 @@ import fs from "fs";
 const folderName = process.argv[2] || "Project";
 
 fs.mkdirSync(folderName);
+
+try {
+  fs.writeFileSync(`${folderName}` / "index.html", "");
+  fs.writeFileSync(`${folderName}` / "app.js", "");
+  fs.writeFileSync(`${folderName}` / "styles.css", "");
+} catch (e) {
+  console.log("Error creating files:", e);
+}
