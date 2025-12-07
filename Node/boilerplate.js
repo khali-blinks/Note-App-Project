68ecc9cc6e5449ff10d336ac3bee978811649b32
Khalifa <kismaiel61@gmail.com>
2025-12-07 08:22:36 -0500

    Testing

--- Node/boilerplate.js
+++ Node/boilerplate.js
@@ -4,9 +4,9 @@ const folderName = process.argv[2] || "Project";
 fs.mkdirSync(folderName);
 
 try {
-  fs.writeFileSync(`${folderName}` / "index.html", "");
-  fs.writeFileSync(`${folderName}` / "app.js", "");
-  fs.writeFileSync(`${folderName}` / "styles.css", "");
+  fs.writeFileSync(`${folderName}/ index.html`, "");
+  fs.writeFileSync(`${folderName}/ app.js`, "");
+  fs.writeFileSync(`${folderName}/ styles.css`, "");
 } catch (e) {
   console.log("Error creating files:", e);
 }
